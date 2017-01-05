adf22a810aa2afda0a8ea5e257aa2ffbd3277dd4
Yasuyuki ENDO <endo@yuras.co.jp>
2017-01-05 17:37:58 +0900

    use Shape3D

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -4,7 +4,12 @@ import org.scijava.java3d.Appearance;
 import org.scijava.java3d.BoundingSphere;
 import org.scijava.java3d.BranchGroup;
 import org.scijava.java3d.DirectionalLight;
+import org.scijava.java3d.Geometry;
+import org.scijava.java3d.GeometryArray;
 import org.scijava.java3d.Material;
+import org.scijava.java3d.PolygonAttributes;
+import org.scijava.java3d.QuadArray;
+import org.scijava.java3d.Shape3D;
 import org.scijava.java3d.Texture2D;
 import org.scijava.java3d.TransformGroup;
 import org.scijava.java3d.utils.behaviors.mouse.MouseRotate;
@@ -13,7 +18,10 @@ import org.scijava.java3d.utils.geometry.Primitive;
 import org.scijava.java3d.utils.image.TextureLoader;
 import org.scijava.java3d.utils.universe.SimpleUniverse;
 import org.scijava.vecmath.Color3f;
+import org.scijava.vecmath.Point2d;
+import org.scijava.vecmath.Point2f;
 import org.scijava.vecmath.Point3d;
+import org.scijava.vecmath.TexCoord2f;
 
 import java.awt.Toolkit;
 import java.awt.Image;
@@ -53,19 +61,35 @@ class Hello {
 
         // material
         Material mat = new Material();
-        mat.setDiffuseColor(new Color3f());
-        mat.setSpecularColor(new Color3f(0.1f, 0.1f, 0.1f));
+        mat.setDiffuseColor(new Color3f(1.0f, 1.0f, 1.0f));
+        mat.setSpecularColor(new Color3f(0.5f, 0.5f, 0.5f));
 
         // appearance
         Appearance ap = new Appearance();
         ap.setMaterial(mat);
         ap.setTexture(tex);
+        //ap.setPolygonAttributes(new PolygonAttributes(PolygonAttributes.POLYGON_FILL, PolygonAttributes.CULL_BACK, 0.0f, false));
 
         // shape
-        Box box = new Box(0.9f, 0.1f, 0.01f,
-                Primitive.GENERATE_TEXTURE_COORDS,
-                ap);
-        trans.addChild(box);
+        Point3d[] verts = new Point3d[4];
+        verts[0] = new Point3d(-0.8, 0.0, 0.0);
+        verts[1] = new Point3d(0.8, 0.0, 0.0);
+        verts[2] = new Point3d(0.8, 0.1, 0.0);
+        verts[3] = new Point3d(-0.8, 0.1, 0.0);
+
+        TexCoord2f[] texcoods = new TexCoord2f[4];
+        texcoods[0] = new TexCoord2f(0.0f, 0.0f);
+        texcoods[1] = new TexCoord2f(1.0f, 0.0f);
+        texcoods[2] = new TexCoord2f(1.0f, 1.0f);
+        texcoods[3] = new TexCoord2f(0.0f, 1.0f);
+
+        QuadArray geom = new QuadArray(verts.length, GeometryArray.COORDINATES | GeometryArray.TEXTURE_COORDINATE_2);
+        geom.setCapability(Geometry.ALLOW_INTERSECT);
+        geom.setCoordinates(0, verts);
+        geom.setTextureCoordinates(0, 0, texcoods);
+        Shape3D shape = new Shape3D(geom, ap);
+        shape.setCapability(Shape3D.ALLOW_GEOMETRY_READ);
+        trans.addChild(shape);
 
         universe.getViewingPlatform().setNominalViewingTransform();
         universe.addBranchGraph(group);
